ea39047515442e0008cc40f833a197b88efe8855
Max Gonzih <gonzih@gmail.com>
2014-10-17 10:13:33 +0200

    Allow to generate picture by using /ping endpoint

--- src-cljs/webcam_ui/core.cljs
+++ src-cljs/webcam_ui/core.cljs
@@ -8,9 +8,13 @@
 
 (enable-console-print!)
 
+(defonce user-id (atom 1))
+
 ;;; Add this: --->
 (let [{:keys [chsk ch-recv send-fn state]}
-      (sente/make-channel-socket! "/chsk" ; Note the same path as before
+     ; Note the same path as before
+      (sente/make-channel-socket! "/chsk"
+                                  :user-id-fn (fn [_] (deref user-id))
        {:type :auto})]
   (def chsk       chsk)
   (def ch-chsk    ch-recv) ; ChannelSocket's receive channel
@@ -18,8 +22,35 @@
   (def chsk-state state))  ; Watchable, read-only atom
 
 (go-loop []
-  (let [response (<! ch-chsk)]
-    (println (keys response))
-    (println (:event response))
-    (println response))
+  (let [event (<! ch-chsk)]
+    (println "==================")
+    (event-msg-handler event))
   (recur))
+
+(defmulti event-msg-handler :id)
+
+(defmethod event-msg-handler :default ; Fallback
+  [{:as ev-msg :keys [event]}]
+  (prn "Unhandled event: %s" event))
+
+(defmethod event-msg-handler :chsk/state
+  [{:as ev-msg :keys [?data]}]
+  (if (= ?data {:first-open? true})
+    (prn "Channel socket successfully established!")
+    (prn "Channel socket state change: %s" ?data)))
+
+(defmethod event-msg-handler :chsk/recv
+  [{:as ev-msg :keys [?data]}]
+  (prn "Push event from server: %s" ?data)
+  (data-handler ?data))
+
+(defmulti data-handler first)
+
+(defmethod data-handler :default
+  [data]
+  (prn "Unhandled data: " data))
+
+(defmethod data-handler :webcam-ui.core/data
+  [[_ data]]
+  (set! (.-src (js/document.getElementById "target"))
+        (:src-data data)))
--- src/webcam_ui/core.clj
+++ src/webcam_ui/core.clj
@@ -1,57 +1,88 @@
-(ns webcam-ui.core
+(ns webcam-ui.core; {{{
   (:require
-    [compojure.core :refer [defroutes GET POST]]
     [webcam-ui.image :refer :all]
     [webcam-ui.screenshot :refer :all]
-    [ring.middleware.file :refer :all]
-    [ring.middleware.reload :as reload]
+    [compojure.core :refer [defroutes GET POST]]
     [compojure.handler :refer [site]]
-    [taoensso.sente :as sente]
-    [org.httpkit.server :refer [run-server]]))
+    [clojure.core.async :as async :refer [go go-loop <! chan put!]]
+    [org.httpkit.server :refer [run-server]]
+    [ring.middleware.file :refer [wrap-file]]
+    [ring.middleware.reload :as reload]
+    [ring.middleware.session :refer [wrap-session]]
+    [ring.util.response :refer [response]]
+    [taoensso.sente :as sente])); }}}
 
-(let [{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn
+(let [{:keys [ch-recv send-fn ajax-post-fn ajax-get-or-ws-handshake-fn; {{{
               connected-uids]}
       (sente/make-channel-socket! {})]
   (def ring-ajax-post                ajax-post-fn)
   (def ring-ajax-get-or-ws-handshake ajax-get-or-ws-handshake-fn)
   (def ch-chsk                       ch-recv) ; ChannelSocket's receive channel
   (def chsk-send!                    send-fn) ; ChannelSocket's send API fn
-  (def connected-uids                connected-uids)) ; Watchable, read-only atom
+  (def connected-uids                connected-uids)) ; Watchable, read-only atom; }}}
 
-(defn layout [yield]
+(defn read-src-data! []; {{{
+  (-> "/tmp/00000001.png"
+      read-file
+      base64->string
+      string->src)); }}}
+
+(def data-chan (chan 1))
+
+(defn start-image-generator! []
+  (future
+    (when (< 0 (count (:any @connected-uids)))
+      (println "Taking screenshot")
+      (take-screenshot!)
+      (put! data-chan (read-src-data!)))))
+
+(defn start-broadcaster! []
+  (println "Starting broadcaster")
+  (go-loop
+    []
+    (let [src (<! data-chan)]
+      (println (format "Broadcasting server>user: %s" @connected-uids))
+      (doseq [uid (:any @connected-uids)]
+        (chsk-send! uid
+                    [::data
+                     {:src-data src}])))
+    (recur)))
+
+(defn layout [yield]; {{{
   (str "<html>
         <head>
           <script src='/javascripts/webcam_ui.js'></script>
         </head>
         <body>"
-       (str @connected-uids)
        yield
        "</doby>
-        </html>"))
+        </html>")); }}}
 
 (defn index-handler [req]
   ; (take-screenshot!)
-  (let [src (-> "/tmp/00000001.png"
-                read-file
-                base64->string
-                string->src)]
-    (layout (str "<img src='" src "'/>"))))
+  (let [src (read-src-data!)]
+    (layout (str "<img id='target' src='" src "'/>"))))
 
-(defroutes main-handler
+(defn ping-handler [req]
+  (start-image-generator!)
+  "hello")
+
+(defroutes main-handler; {{{
   (GET "/" [] index-handler)
+  (GET "/ping" [] ping-handler)
   (GET  "/chsk" req (ring-ajax-get-or-ws-handshake req))
-  (POST "/chsk" req (ring-ajax-post                req)))
+  (POST "/chsk" req (ring-ajax-post                req))); }}}
 
-(def app (wrap-file main-handler "resources/public"))
-
-(future
-  (map #(chsk-send! % {:test :test}) @connected-uids)
-  (Thread/sleep 3000))
+(def app (-> main-handler
+             (wrap-file "resources/public")
+             wrap-session))
 
 (defn in-dev? [_] true)
 
 (defn -main [& args] ;; entry point, lein run will pick up and start from here
+  (start-broadcaster!)
   (let [handler (if (in-dev? args)
                   (reload/wrap-reload (site #'app)) ;; only reload when dev
                   (site app))]
+    (println "Running web server")
     (run-server handler {:port 8080})))
